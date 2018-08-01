7e6d4ea37ead513f3660f3755fa2968314fb89e0
jesus2102 <jesusperezmarin@gmail.com>
2018-08-01 17:29:15 +0100

    shouldGetTotalDistance passed

--- models/traveller.js
+++ models/traveller.js
@@ -28,7 +28,10 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const totalDistance = this.journeys.reduce((total, journey) => {
+    return total + journey.distance;
+  }, 0);
+  return totalDistance;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
